4499a000271b93da05781d4a3f00f94be45fe2f8
Phillip Strom <phillip.strom@gmail.com>
2021-03-05 11:49:24 -0700

    Change landing to welcome

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe 'Landing Page Index Page Spec' do
-  it 'I can visit the landing page' do
+RSpec.describe 'Welcome Page Index Page Spec' do
+  it 'I can visit the welcome page' do
     visit root_path
     expect(page).to have_content('Welcome to DQS Calc!')
   end
