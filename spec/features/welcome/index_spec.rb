ea5fea60ed364411e353abb3b7f811b1ff6fffb4
Phillip Strom <phillip.strom@gmail.com>
2021-03-05 11:58:15 -0700

    Add test for registration link

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -1,8 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe 'Welcome Page Index Page Spec' do
+RSpec.describe 'Welcome Page Index Page' do
   it 'I can visit the welcome page' do
     visit root_path
     expect(page).to have_content('Welcome to DQS Calc!')
+  end
+  it 'has a user registration link' do
+    visit root_path
+    expect(page).to have_link('Sign up')
   end
 end
