f9d84a01805909344dd053298a7ae9081fb64d6c
Phillip Strom <phillip.strom@gmail.com>
2021-03-08 14:57:45 -0700

    Add new user spec

--- spec/features/user/registration_spec.rb
+++ spec/features/user/registration_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe 'User Registration' do
   it 'I can get to registration form from Welcome' do
-
+    visit root_path
+    click_on 'Sign up'
+    expect(current_path).to eq(new_user_path)
+    expect(page).to have_field('Username')
+    expect(page).to have_field('Password')
   end
 end
