665792f078e38b4e6998fedc49efcf220c7de48e
Phillip Strom <phillip.strom@gmail.com>
2021-03-08 16:28:45 -0700

    Updatea email attributes to username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  validates :email, uniqueness: true, presence:true
+  validates :username, uniqueness: true, presence:true
   validates_presence_of :password_digest, require: true
 
   has_secure_password
-  
+
 end
