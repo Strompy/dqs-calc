eb6b045b7796e810d52f998a048d2c48c648eca8
Phillip Strom <phillip.strom@gmail.com>
2021-03-09 14:44:21 -0700

    Remove duplicate password confirmation validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
-  validates_confirmation_of :password
   validates :username, uniqueness: true, presence:true
   validates_presence_of :password_digest, require: true
-  has_secure_password
 
+  has_secure_password
 end
