dc791a6029a055da80d4caefb65fc6754a11933f
Phillip Strom <phillip.strom@gmail.com>
2021-03-09 14:44:46 -0700

    Add test for non-unique username

--- spec/features/user/registration_spec.rb
+++ spec/features/user/registration_spec.rb
@@ -37,5 +37,26 @@ RSpec.describe 'User Registration' do
     expect(find_field(:user_username).value).to eq('Gwen')
     expect(find_field(:user_password).value).to be nil
     expect(find_field(:user_password_confirmation).value).to be nil
+    expect(page).to have_content("Password confirmation doesn't match Password")
+
+  end
+  it 'I cannot create a user with non-unqiue username' do
+    user = User.create!(username: 'Gwen', password: 'tri')
+    expect(User.count).to eq(1)
+
+    visit new_user_path
+    fill_in 'user_username', with: 'Gwen'
+    fill_in 'user_password', with: 'password1'
+    fill_in 'user_password_confirmation', with: 'password1'
+    click_on 'Submit'
+
+    expect(User.count).to eq(1)
+
+    expect(current_path).to eq(users_path)
+    expect(find_field(:user_username).value).to eq('Gwen')
+    expect(find_field(:user_password).value).to be nil
+    expect(find_field(:user_password_confirmation).value).to be nil
+
+    expect(page).to have_content('Username has already been taken')
   end
 end
