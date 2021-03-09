6b75ccdb6c7a127ff900416dbd9909d733665f6f
Phillip Strom <phillip.strom@gmail.com>
2021-03-09 14:04:43 -0700

    Add user registration test

--- spec/features/user/registration_spec.rb
+++ spec/features/user/registration_spec.rb
@@ -9,4 +9,17 @@ RSpec.describe 'User Registration' do
     expect(page).to have_field('user_password')
     expect(page).to have_field('user_password_confirmation')
   end
+  it 'I can create a new user' do
+    visit new_user_path
+    fill_in 'user_username', with: 'Gwen'
+    fill_in 'user_password', with: 'password'
+    fill_in 'user_password_confirmation', with: 'password'
+    click_on 'Submit'
+
+    user = User.first
+    expect(user.username).to eq 'Gwen'
+    expect(user.password_digest).to_not be nil
+
+    expect(current_path).to eq(user_path(user.id))
+  end
 end
