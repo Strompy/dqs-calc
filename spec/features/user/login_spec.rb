0851bae56b96339b906e15ed7c5414b627bdf339
Phillip Strom <phillip.strom@gmail.com>
2021-03-10 11:58:08 -0700

    Create sad path login test

--- spec/features/user/login_spec.rb
+++ spec/features/user/login_spec.rb
@@ -15,4 +15,25 @@ RSpec.describe 'User Login' do
     expect(current_path).to eq(user_path(user.id))
     expect(page).to have_content("Welcome, #{user.username}!")
   end
+  it 'I cannot log in with bad credentials' do
+    user = User.create!(username: 'Gwen', password: 'supersecret')
+
+    visit root_path
+    click_on 'Login'
+    expect(current_path).to eq(new_session_path)
+
+    fill_in 'user_username', with: 'fakename'
+    fill_in 'user_password', with: user.password
+    click_on 'Log in'
+
+    expect(current_path).to_not eq(user_path(user.id))
+    expect(page).to have_content('Incorrect username or password')
+
+    fill_in 'user_username', with: user.username
+    fill_in 'user_password', with: 'fakepassword'
+    click_on 'Log in'
+
+    expect(current_path).to_not eq(user_path(user.id))
+    expect(page).to have_content('Incorrect username or password')
+  end
 end
