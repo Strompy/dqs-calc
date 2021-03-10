d4165680bfed6fe6794c6ddefe8100a6fe8591de
Phillip Strom <phillip.strom@gmail.com>
2021-03-10 11:59:10 -0700

    Change @user to only be used for sad path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,11 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(username: user_params[:username])
-    if @user != nil && @user.authenticate(user_params[:password])
-      session[:user_id] = @user.id
-      flash[:success] = "Welcome, #{@user.username}!"
-      redirect_to user_path(@user.id)
+    user = User.find_by(username: user_params[:username])
+    if user != nil && user.authenticate(user_params[:password])
+      session[:user_id] = user.id
+      flash[:success] = "Welcome, #{user.username}!"
+      redirect_to user_path(user.id)
     else
       flash[:error] = 'Incorrect username or password. Please try again or sign up for an account.'
       @user = User.new(username: user_params[:username])
