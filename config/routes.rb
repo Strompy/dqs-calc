34d1794db31d11282a488093b577c4145792d0d0
Phillip Strom <phillip.strom@gmail.com>
2021-03-11 12:50:46 -0700

    Add entries resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
-  resources :users, only: [:show, :new, :create]
+  resources :users, only: [:show, :new, :create] do
+    resources :entries, only: [:new, :create]
+  end
   resources :sessions, only: [:new, :create]
 end
