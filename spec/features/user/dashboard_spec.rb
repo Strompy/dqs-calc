18bf0d9ba26525b2cc1ba4c10c69c3358ec6910a
Phillip Strom <phillip.strom@gmail.com>
2021-03-11 12:50:19 -0700

    Fix filename typo

--- spec/features/user/dashboard_spec.rb
+++ spec/features/user/dashboard_spec.rb
@@ -9,15 +9,12 @@ RSpec.describe 'User Dashboard/Show Page' do
     click_on 'Log in'
 
     expect(current_path).to eq(user_path(user.id))
-
     expect(page).to have_content("#{user.username}'s dashboard")
     expect(page).to have_css('.today')
     expect(page).to have_css('.recommended')
     expect(page).to have_css('.stats')
-
   end
   it 'As user I can add entries from the dashboard' do
-    skip
     user = User.create!(username: 'Gwen', password: 'supersecret')
     visit new_session_path
     fill_in 'user_username', with: user.username
@@ -29,6 +26,6 @@ RSpec.describe 'User Dashboard/Show Page' do
     expect(page).to have_link('Add Food')
     click_on 'Add Food'
 
-    expect(current_path).to eq(new_entry_path)
+    expect(current_path).to eq(new_user_entry_path)
   end
 end
