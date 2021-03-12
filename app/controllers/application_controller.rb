8de0ed2544bcae411dd225d8ec60687553b3c0ac
Phillip Strom <phillip.strom@gmail.com>
2021-03-11 18:36:27 -0700

    Create current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user #makes method available to view
+
+  def current_user
+    unless session[:user_id].nil?
+      @user || User.find(session[:user_id])
+    end
+  end
 end
