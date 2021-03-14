26e6a9f53e3951f83da8e6cd038dadd5999f1356
Phillip Strom <phillip.strom@gmail.com>
2021-03-13 18:11:37 -0700

    Migrate entries

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_08_232045) do
+ActiveRecord::Schema.define(version: 2021_03_14_010722) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "entries", force: :cascade do |t|
+    t.string "meal_type"
+    t.string "category"
+    t.decimal "serving"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_entries_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -22,4 +32,5 @@ ActiveRecord::Schema.define(version: 2021_03_08_232045) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "entries", "users"
 end
