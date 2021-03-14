34d2f5ad40ff164b3c4890e184780433577aa028
Phillip Strom <phillip.strom@gmail.com>
2021-03-13 18:59:17 -0700

    Add create action

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -2,6 +2,24 @@ class EntriesController < ApplicationController
   before_action :require_current_user
 
   def new
-    require "pry"; binding.pry
+    @user = User.find(params[:user_id])
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    entry = @user.entries.new(entry_params)
+    if entry.save
+      flash[:success] = 'New entry was successfully created'
+      redirect_to dashboard_index_path
+    else
+      flash[:error] = @user.errors.full_messages.to_sentence
+      render :new
+    end
+  end
+
+  private
+
+  def entry_params
+    params.require(:entry).permit(:meal_type, :category, :serving)
   end
 end
