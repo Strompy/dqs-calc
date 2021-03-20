b58c533321755bb308859c4245a6c9048478edce
Phillip Strom <phillip.strom@gmail.com>
2021-03-20 14:35:59 -0600

    Create test for today_by_category method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,4 +9,25 @@ RSpec.describe User, type: :model do
   describe 'relationships' do
     it { should have_many :entries }
   end
+
+  describe 'instance_methods' do
+    it 'can find todays entryies sorted by category per meal_type' do
+      user = User.create!(username: 'Gwen', password: 'supersecret')
+      user.entries.create!(meal_type: 'Breakfast', category: 'Fruits', serving: 1.5)
+      user.entries.create!(meal_type: 'Breakfast', category: 'Fruits', serving: 1)
+      user.entries.create!(meal_type: 'Lunch', category: 'Vegetables', serving: 1)
+
+      result = user.today_by_category('Breakfast')
+      expected = {"Fruits" => 2.5}
+      expect(result).to eq(expected)
+
+      result = user.today_by_category('Lunch')
+      expected = {"Vegetables" => 1}
+      expect(result).to eq(expected)
+
+      result = user.today_by_category('Dinner')
+      expected = {}
+      expect(result).to eq(expected)
+    end
+  end
 end
