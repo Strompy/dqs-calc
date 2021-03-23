9a64d11549bd7cbad8b01bc13a0cf4ffa2498d74
Phillip Strom <phillip.strom@gmail.com>
2021-03-23 12:18:27 -0600

    Add AR call and switch case outline

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,33 @@ class User < ApplicationRecord
       .group("category")
       .sum("serving")
   end
+
+  def dqs
+    require "pry"; binding.pry
+    # grab todays entries grouped by category
+    groups = entries.where(created_at: Date.today.all_day)
+    # iterate through groups to calculate based on category
+    groups.each do |category, quantity|
+      case category
+      when 'Fruits', 'Vegetables'
+        # helper method?
+      when 'High-Quality Meat/Seafoods', 'Legumes, Plant Protein', 'Nuts, Seeds, Healthy Oils', 'Whole Grains'
+        # helper method?
+      when 'Dairy'
+        # helper method?
+      when 'High-Quality Beverages'
+        # helper method?
+      when 'High-Quality Processed Foods'
+        # helper method?
+      when 'Refined Grains', 'Low-Quality Meat/Seafoods'
+        # helper method?
+      when 'Sweets', 'Fried Foods', 'Low-Quality Beverages'
+        # helper method?
+      when 'Low-Quality Meat/Seafoods'
+        # helper method?
+      end
+    end
+    # case statement may be most helpful
+
+  end
 end
