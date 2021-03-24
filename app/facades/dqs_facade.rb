1ae790bc9d28e867a4c981a44f8b3f98ab23ced4
Phillip Strom <phillip.strom@gmail.com>
2021-03-24 15:15:52 -0600

    Add finished poro method call

--- app/facades/dqs_facade.rb
+++ app/facades/dqs_facade.rb
@@ -1,9 +1,9 @@
 class DqsFacade
   def self.current_dqs(user)
     # get entry data from model
-    require "pry"; binding.pry
-    user.sum_today_by_category
+    entries_hash = user.sum_today_by_category
     # use poro to calculate score
-    # return dqs number
+    # return score
+    DqsPoro.new.calculate(entries_hash)
   end
 end
