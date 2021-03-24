c066d4d8c0da584cc98cb06eed11f679e4fdbc13
Phillip Strom <phillip.strom@gmail.com>
2021-03-24 15:14:36 -0600

    Remove old comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,31 +13,9 @@ class User < ApplicationRecord
   end
 
   def sum_today_by_category
-    # grab todays entries grouped by category
-    require "pry"; binding.pry
+    # rework the names for these methods
+    # require "pry"; binding.pry
     entries.where(created_at: Date.today.all_day).group("category").sum("serving")
-    # iterate through groups to calculate based on category
-    # groups.each do |category, quantity|
-    #   case category
-    #   when 'Fruits', 'Vegetables'
-    #     # helper method?
-    #     # score += fruit_veg_calc(quantity)
-    #   when 'Lean Meats & Fish', 'Nuts & Seeds', 'Whole Grains'
-    #     # helper method?
-    #     # score += meat_seeds_grains_calc(quantity)
-    #   when 'Dairy'
-    #     # helper method?
-    #     # score += dairy_calc(quantity)
-    #   when 'Refined Grains', 'Fatty Proteins'
-    #     # helper method?
-    #     # score += refined_grains_fatty_calc(quantity)
-    #   when 'Sweets', 'Fried Foods'
-    #     # helper method?
-    #     # score += fried_sweets_calc(quantity)
-    #   # add else?
-    #   end
-    # end
-
   end
 
 
