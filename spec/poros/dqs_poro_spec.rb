b03b4e9fdddfe4e57d0208366ce651b56c885a92
Phillip Strom <phillip.strom@gmail.com>
2021-03-26 12:48:11 -0600

    Add edge case tests for dqs calculate

--- spec/poros/dqs_poro_spec.rb
+++ spec/poros/dqs_poro_spec.rb
@@ -29,7 +29,30 @@ RSpec.describe DqsPoro do
 
     expect(user.entries.size).to eq(3)
     entries_hash = user.sum_today_by_category
-    expect(poro.calculate(entries_hash)).to eq(7)
+    expect(poro.calculate(entries_hash)).to eq(7.0)
   end
+  it 'can calculate dqs with large servings' do
+    poro = DqsPoro.new
+    user = User.create!(username: 'Gwen', password: 'supersecret')
+    user.entries.create!(meal_type: 'Breakfast', category: 'Fruits', serving: 1.5)
+    user.entries.create!(meal_type: 'Breakfast', category: 'Fruits', serving: 1)
+    user.entries.create!(meal_type: 'Lunch', category: 'Vegetables', serving: 1)
+    user.entries.create!(meal_type: 'Lunch', category: 'Nuts & Seeds', serving: 8)
 
+    entries_hash = user.sum_today_by_category
+    expect(poro.calculate(entries_hash)).to eq(9.0)
+
+    user2 = User.create!(username: 'Gwen2', password: 'supersecret2')
+    user2.entries.create!(meal_type: 'Breakfast', category: 'Fried Foods', serving: 10)
+
+    entries_hash = user2.sum_today_by_category
+    expect(poro.calculate(entries_hash)).to eq(-20.0)
+  end
+  it 'can calculate dqs with no servings' do
+    poro = DqsPoro.new
+    user = User.create!(username: 'Gwen', password: 'supersecret')
+
+    entries_hash = user.sum_today_by_category
+    expect(poro.calculate(entries_hash)).to eq(0.0)
+  end
 end
