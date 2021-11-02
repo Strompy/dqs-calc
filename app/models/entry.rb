d3fb1f34f78aa3ef85a85119464286c51ce00c19
Phillip Strom <phillip.strom@gmail.com>
2021-11-01 21:11:27 -0600

    Refactor entry categories

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -6,4 +6,5 @@ class Entry < ApplicationRecord
   belongs_to :user
 
   MEAL_TYPES = ['Breakfast', 'Lunch', 'Dinner', ' Snacks', 'Beverages']
+  CATEGORIES = ['Fruits', 'Vegetables', 'Lean Meats & Fish', 'Nuts & Seeds', 'Whole Grains', 'Dairy', 'Refined Grains', 'Sweets', 'Fried Foods', 'Fatty Proteins']
 end
